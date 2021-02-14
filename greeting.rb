4c4fd98f8e855c079d855c954a00a0bd8595be37
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-14 03:04:52 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,47 @@
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+ 
+say_hello_world_ten_times
+
+
+
+def line
+  katz_deli.size
+  katz_deli.each { |name| 
+    puts "1. #{[0]}, 2. "
+  }
+  
+  #use .each for this, Omar.  And need to somehow match each name with its index number + 1, for puts; maybe return a new_array listing, for example: "1. Ada.  2. Grace..."
+  if
+    puts "The line is currently: #{new_array}." # Note that the example sentence they provide does not end with  a sentence.
+  else
+    puts "The line is currently empty."
+end
+
+
+def take_a_number(katz_deli, name)
+  # add name to end of array, Omar.
+  puts "Welcome, #{name}. You are number #{katz_deli[-1]} in line."
+end
+
+def now_serving
+  if katz_deli != [ ]
+    puts "Currently serving #{katz_deli[0]}." 
+    # remove first member of array, Omar.
+
+  
+  else
+    puts "There is nobody waiting to be served!"
+end
+    
